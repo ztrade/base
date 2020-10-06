28ceb84a9bc1b94fe55e0b411897b733d4c86e14
Super <yiyangzhi111@gmail.com>
2020-10-06 20:44:09 +0800

    add engine module

---
+++ engine/interface.go
@@ -0,0 +1,30 @@
+package engine
+
+import (
+	"github.com/SuperGod/indicator"
+	"github.com/SuperGod/trademodel"
+	"github.com/ztrade/base/common"
+)
+
+type Engine interface {
+	OpenLong(price, amount float64)
+	CloseLong(price, amount float64)
+	OpenShort(price, amount float64)
+	CloseShort(price, amount float64)
+	StopLong(price, amount float64)
+	StopShort(price, amount float64)
+	CancelAllOrder()
+	AddIndicator(name string, params ...int) (ind indicator.CommonIndicator)
+	Position() (pos, price float64)
+	Balance() float64
+	Log(v ...interface{})
+	Watch(watchType string)
+	SendNotify(content, contentType string)
+	Merge(src, dst string, fn common.CandleFn)
+	SetBalance(balance float64)
+
+	// call for goscript
+	UpdatePosition(pos, price float64)
+	OnCandle(candle trademodel.Candle)
+	UpdateBalance(balance float64)
+}
